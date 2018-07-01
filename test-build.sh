dcdf1958a235d0d2c6d7ec065386b0939474c9ed
Christopher Horrell <christopher@horrell.ca>
2018-06-30 21:26:33 -0400

    Skip npm and yarn tests for "core" variant

--- test-build.sh
+++ test-build.sh
@@ -54,6 +54,7 @@ function test_image() {
 
   info "Testing ${full_tag}"
   export full_version=${full_version}
+  export variant=${variant}
   export full_tag=${full_tag}
   bats test-image.bats
 }
